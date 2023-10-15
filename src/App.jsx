208754515b7bed8e9d6290bb79c9137b16ccc9d2
Ashish Munot <ashish.munot22@gmail.com>
2023-10-15 18:48:06 +0530

    Implemented basic folder structure whith a Home page and non functioning Navbar

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Home from "./features/Pages/Home"
+
+const App = () => {
+  return <Home></Home>
+}
+
+export default App
---
+++ src/features/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../NavBar/NavBar"
+import ProductList from "../ProductList/ProductList"
+
+const Home = () => {
+  return (
+    <NavBar>
+      <ProductList />
+    </NavBar>
+  )
+}
+
+export default Home
